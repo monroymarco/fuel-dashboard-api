2dc0a6aaf7581159c2038f5972171156d37131f7
Marco Monroy <marcomonroy@outlook.com>
2026-09-08 13:26:18 +0200

    Configurar CORS para Vercel

--- main.py
+++ main.py
@@ -11,7 +11,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "https://spain-fuel-prices-dashboard-cyl1km6ai-skynet23.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
